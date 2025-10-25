d74b94fdb5bf7370d740a324378ed330f15abf33
ossianws <monsieuralfonse64@gmail.com>
2025-10-25 13:27:42 +0100

    added readme

--- app/visuals.py
+++ app/visuals.py
@@ -6,12 +6,15 @@ def generate_graph(id):
     data_dict = graph_list[id]
     graph_type=data_dict['type']
     data = data_dict['data']
+    
     if graph_type == 'scatter':
         fig = go.Figure(data=go.Scatter(x=data[0].tolist(), y=data[1].tolist(), mode = "markers"))
         fig.update_layout(title=data_dict['title'], xaxis_title='X Axis', yaxis_title='Y Axis')
+
     elif graph_type == 'bar':
         fig = go.Figure(data=go.Bar(x=data[0].tolist(), y=data[1].tolist()))
         fig.update_layout(title=data_dict['title'], xaxis_title='X Axis', yaxis_title='Y Axis')
+
     elif graph_type == "pie":
         fig = go.Figure(data=go.Pie(labels=data.tolist()))
         fig.update_layout(title=data_dict['title'])
